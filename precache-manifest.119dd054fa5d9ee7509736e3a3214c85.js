602f7e0be9a3dbae3197f84239d31347d4e58506
Fernando <f.sanchezvilela@gmail.com>
2018-11-12 18:36:35 +0100

    Updates

---
+++ precache-manifest.119dd054fa5d9ee7509736e3a3214c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ec6f1c22d2ebd0db239",
+    "url": "/cmredux/static/js/runtime~main.7ec6f1c2.js"
+  },
+  {
+    "revision": "ff1662c2ac9fc9fbc180",
+    "url": "/cmredux/static/js/main.ff1662c2.chunk.js"
+  },
+  {
+    "revision": "fa89a4db309d612b9ea3",
+    "url": "/cmredux/static/js/1.fa89a4db.chunk.js"
+  },
+  {
+    "revision": "ff1662c2ac9fc9fbc180",
+    "url": "/cmredux/static/css/main.a41f0652.chunk.css"
+  },
+  {
+    "revision": "fa89a4db309d612b9ea3",
+    "url": "/cmredux/static/css/1.8fefd672.chunk.css"
+  },
+  {
+    "revision": "fc7ccf01732583e7dc54439a7f4c1c09",
+    "url": "/cmredux/index.html"
+  }
+];
